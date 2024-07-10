e4d6f79de1026916bdda23b55c889ed3819c0639
Liron-Refaeli <77193360+Liron-Refaeli@users.noreply.github.com>
2024-07-10 19:32:34 +0300

    Update ec2.tf

--- terraform/aws/ec2.tf
+++ terraform/aws/ec2.tf
@@ -278,7 +278,7 @@ resource "aws_s3_bucket" "flowbucket" {
     }, {
     git_commit           = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
     git_file             = "terraform/aws/ec2.tf"
-    git_last_modified_at = "2020-06-16 14:46:24"
+    git_last_modified_at = "2020-06-16 14:46:22"
     git_last_modified_by = "nimrodkor@gmail.com"
     git_modifiers        = "nimrodkor"
     git_org              = "bridgecrewio"
